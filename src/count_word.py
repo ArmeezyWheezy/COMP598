f55667f89e692c92106ea7af3fb7e6ac607153c9
404NotFo <Muzhi.qi@mail.mcgill.ca>
2021-12-12 13:05:42 -0500

    update

--- src/count_word.py
+++ src/count_word.py
@@ -14,20 +14,22 @@ for i in stopfile:
     stopwords.append(i)
 
 
-csvfile = open(datapath, "r", encoding="utf8")
-csv_reader = reader(csvfile)
+while True:
 
-topic = ["Restrictions","Demography","Impact","Vaccine","Variant","Symptoms","Testing","Other"]
-sentiment = ["Positive","Negative","Neutral"]
+    csvfile = open(datapath, "r", encoding="utf8")
+    csv_reader = reader(csvfile)
+
+    topic = ["Restrictions","Demography","Impact","Vaccine","Variant","Symptoms","Testing","Other"]
+    sentiment = ["Positive","Negative","Neutral"]
 
 
-result = {"Positive":0,"Negative":0,"Neutral":0}
-top = input("Enter the topic(Restrictions,Demography,Impact,Vaccine,Variant,Symptoms,Testing,Other)\t")
-word = input("Enter word\t")
-for i in csv_reader:
-    a = re.sub("[^a-zA-Z0-9]", " ", i[3]).lower()
-    j = a.split(" ")
-    if word in j:
-        result[i[5]] += 1
+    result = {"Positive":0,"Negative":0,"Neutral":0}
+    top = input("Enter the topic(Restrictions,Demography,Impact,Vaccine,Variant,Symptoms,Testing,Other)\t")
+    word = input("Enter word\t")
+    for i in csv_reader:
+        a = re.sub("[^a-zA-Z0-9]", " ", i[3]).lower()
+        j = a.split(" ")
+        if word in j:
+            result[i[5]] += 1
 
-print(result)
+    print(result)
